d1eeedef5329a1114655de8cda47ab70c2ad3736
Boas van Voorst <modelspoorfan@outlook.com>
2017-12-05 13:42:11 +0100

    Roommapper2
    Boas van Voorst

--- app/src/main/java/jabo/com/roommapper2/Screens/menu.java
+++ app/src/main/java/jabo/com/roommapper2/Screens/menu.java
@@ -10,31 +10,10 @@ import jabo.com.roommapper2.TCP.TcpClient;
 
 public class menu extends Activity {
 
-    private TcpClient mTcpClient;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
 
-    public class ConnectTask extends AsyncTask<String, String, TcpClient> {
-
-        @Override
-        protected TcpClient doInBackground(String... message) {
-            //we create a TCPClient object and
-            mTcpClient = new TcpClient(new TcpClient.OnMessageReceived() {
-                @Override
-                //here the messageReceived method is implemented
-                public void messageReceived(String message) {
-                    //this method calls the onProgressUpdate
-                    //publishProgress(message);
-                    Log.i("Debug","Input message: " + message);
-                }
-            });
-            mTcpClient.run();
-
-            return null;
-        }
-    }
 }
